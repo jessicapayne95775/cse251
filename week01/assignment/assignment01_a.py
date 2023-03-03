9bd0783db594dd1560d6276076074cc3cd16cf81
jessicapayne95775 <jen19007@byui.edu>
2023-03-03 10:45:43 -0700

    assignment 1

--- week01/assignment/assignment01_a.py
+++ week01/assignment/assignment01_a.py
@@ -13,7 +13,6 @@ Psuedocode:
 4. Using assert, check the expected result (see main)
 5. Repeat steps 3 and 4, but use 13.
 6. Repeat steps 3 and 4, but use 17.
-
 Things to consider:
 a. If using a global, what is the correct syntax for creating a thread with one argument?
    (see https://stackoverflow.com/questions/3221655/python-threading-string-arguments)
@@ -21,53 +20,32 @@ b. How do you start a thread? (see this week's reading)
 c. How will you wait until the thread is done? (see this week's reading)
 d. Do threads (including the main thread) share global variables? (see https://superfastpython.com/thread-share-variables/)
 e. If you use a global, how will you ensure that one thread doesn't change the value of
-   your global while another thread is using it too? (We haven't learned about locks yet, so you
-   won't be able to run your threads simultaneously)
+   your global while another thread is using it too? (We haven't learned about locks yet, so you won't be able to run your threads simultaneously)
 f. How do you modify the value of a global variable (see https://stackoverflow.com/questions/10588317/python-function-global-variables)
 g. If using a list object, how to you instantiate it with the correct number of indexes? (see https://stackoverflow.com/questions/8528178/list-of-zeros-in-python)
 '''
-import threading
-
-# global sum
-SUM = 0
-
-
-def summing(index, number, results):
-    sum = 0
-    for i in range(number):
-        sum += i
-        print(f'{index=}, {i=}, {sum=}\n', end="")
-    results[index] = sum
-
-
+import threading 
+def sum(index, number, results): #create the function the threads will do 
+   sum = 0   # set the sum to 0 to start
+   for num in range(number): # add each number up to the specificed number
+      sum += num
+   results.insert(index, sum) #insert sum total into an array at the index based on the thread number
+   return results # return the answer array to use data elsewhere
 def main():
-
-    # If not using a global, use this list to store your results
-    results = [0] * 3
-
-    threads = []
-    threads.append(threading.Thread(target=summing, args=(0, 10, results)))
-    threads.append(threading.Thread(target=summing, args=(1, 13, results)))
-    threads.append(threading.Thread(target=summing, args=(2, 17, results)))
     
-    for t in threads:
-        t.start()
-        
-    for t in threads:
-        t.join()
-
-    # If using a global, place these asserts after the call to your summing function
-    # to check that your code is working correctly
-    #assert SUM == 45, f'The sum should equal 45 but instead was {SUM}'
-    #assert SUM == 78, f'The sum should equal 78 but instead was {SUM}'
-    #assert SUM == 136, f'The sum should equal 136 but instead was {SUM}'
-    # If using a list object, place these asserts after the call to your summing function
-    # to check that your code is working correctly
-    assert results[0] == 45, f'The sum should equal 45 but instead was {results[0]}'
-    assert results[1] == 78, f'The sum should equal 78 but instead was {results[1]}'
-    assert results[2] == 136, f'The sum should equal 136 but instead was {results[2]}'
-
-
-if __name__ == '__main__':
+    # If not using a global, use this list to store your results
+    results = [] # form results array and make it an empty list to start
+    thread1 = threading.Thread(target=sum, args=(0, 10, results)) #create a thread that used the sum function and passes in index, number, and the results array
+    thread2 = threading.Thread(target=sum, args=(1, 13, results))
+    thread3 = threading.Thread(target=sum, args=(2, 17, results))
+    threads = [thread1, thread2, thread3] # put all the threads in an array for easy .start() and .join() functions
+    for t in threads: # run the threads through .start() and end them with .join()
+      t.start()
+      t.join()
+      print(results) #printed to debug
+    assert results[0] == 45, f'The sum should equal 45 but instead was {results[0]}' # test thread1
+    assert results[1] == 78, f'The sum should equal 78 but instead was {results[1]}' # test thread2
+    assert results[2] == 136, f'The sum should equal 136 but instead was {results[2]}' # test thread3
+if __name__ == '__main__': #makes sure the file that is running is the main file.
     main()
-    print("DONE")
+    print("DONE") #Shows that the program ran and finished without errors
--- week01/assignment/assignment01_b.py
+++ week01/assignment/assignment01_b.py
@@ -1,75 +1,70 @@
 import threading
 '''
-Requirements:Create a class that extends the 'threading.Thread'
-1.  class (see https://stackoverflow.com/questions/15526858/how-to-extend-a-class-in-python). This means that the class IS a thread. 
+Requirements:
+1. Create a class that extends the 'threading.Thread' class (see 
+https://stackoverflow.com/questions/15526858/how-to-extend-a-class-in-python). This
+means that the class IS a thread. 
    Any objects instantiated using this class ARE threads.
 2. Instantiate this thread class that computes the sum of all numbers 
    between one and that number (exclusive)
-
 Psuedocode:
-1. In your class, write a constructor (in python a constructor is __init__) and allow a number
+1. In your class, write a constructor (in python a constructor is __init__) and 
+allow a number
    to be passed in as a parameter.
 2. The constructor should call the parent class's constructor:
    threading.Thread.__init__(self)
 3. Create a local sum variable in your constructor.
-4. A thread must have a run function, so create a run function that sums from one to the 
+4. A thread must have a run function, so create a run function that sums from one 
+to the 
    passed in number (inclusive).
 5. In the run function, set the sum on self.
 6. In main, instantiate your thread class with the a value of 10.
 7. Start the thread.
 8. Wait for the thread to finish.
-9. Assert that thread object's sum attribute is equal to the appropriate value (see main).
+9. Assert that thread object's sum attribute is equal to the appropriate value (see
+main).
 10. Repeat steps 7 through 10 using a value of 13.
 11. Repeat steps 7 through 10 using a value of 17.
-
 Things to consider:
-a. How do you instantiate a class and pass in arguments (see https://realpython.com/lessons/instantiating-classes/)?
+a. How do you instantiate a class and pass in arguments (see 
+https://realpython.com/lessons/instantiating-classes/)?
 b. How do you start a thread object (see this week's reading)?
 c. How will you wait until the thread is done (see this week's reading)?
-d. How do you get the value an object's attribute (see https://datagy.io/python-print-objects-attributes/)?
+d. How do you get the value an object's attribute (see https://datagy.io/python-
+print-objects-attributes/)?
 '''
 ######################
 # DO NOT USE GLOBALS #
 ######################
-
-class SummingNumbers(threading.Thread):
-    '''Sums numbers'''
-    def __init__(self, number):
-        threading.Thread.__init__(self)
-        self.sum = 0
-        self.number = number
-
-    def run(self):
-        for x in range(self.number):
-            self.sum = self.sum + x
-            #print(f'{self.sum=}')
-            
+import threading
+# TODO - Create a thread class
+class Sum_thread(threading.Thread): #extends threading to class
+      def __init__(self, number):   #constructs the class with itself and number as a parameter
+         threading.Thread.__init__(self) #You must initiate the super class
+         self.number = number          # defining parameter
+         self.sum = 0                  #defining variable
+      def run(self):                   #override the built-in run function; doing what we want it to
+         for num in range(self.number):   #Loop all the numbers to the declared number
+            self.sum += num            #add each number to the total sum until declared number
+ 
 def main():
-    
-    t1 = SummingNumbers(10)
-    t2 = SummingNumbers(13)
-    t3 = SummingNumbers(17)
-    
-    t1.start()
-    t2.start()
-    t3.start()
-    
-    t1.join()
-    t2.join()
-    t3.join()
-    
-    # Instantiate your thread class and pass in 10.
-    # Test (assert) if its sum attribute is equal to 45.
-    # Note: do no use 'yourThread' for the name of your thread object
-    assert t1.sum == 45, f'The sum should equal 45 but instead was {t1.sum}'
-    
-    # Repeat, passing in 13
-    assert t2.sum == 78, f'The sum should equal 78 but instead was {t2.sum}'
-    
-    # Repeat, passing in 17
-    assert t3.sum == 136, f'The sum should equal 136 but instead was {t3.sum}'
-
-if __name__ == '__main__':
+   # Instantiate your thread class and pass in 10.
+   thread1 = Sum_thread(10) #store object in variable and pass 10 into the number slot in the class
+   thread2 = Sum_thread(13) #store object in variable and pass 13 into the number slot in the class
+   thread3 = Sum_thread(17) #store object in variable and pass 17 into the number slot in the class
+   thread1.start()   #starts the thread. Do not use run().
+   thread2.start()
+   thread3.start()
+   thread1.join()    #Ensures the thread finishes before moving on to the next set of code
+   # Test (assert) if its sum attribute is equal to 45.
+   assert thread1.sum == 45, f'The sum should equal 45 but instead was {thread1.sum}'
+   thread2.join()
+   # Repeat, passing in 13
+   assert thread2.sum == 78, f'The sum should equal 78 but instead was {thread2}'
+   thread3.join()
+   # Repeat, passing in 17
+   assert thread3.sum == 136, f'The sum should equal 136 but instead was {thread3}'
+if __name__ == '__main__': #makes sure the file that is running is the main file.
     main()
-    assert threading.active_count() == 1
-    print("DONE")
+    assert threading.active_count() == 1 #tests to see if there are 1 thread running actively
+    print("DONE") #Shows that the program ran and finished without errors
