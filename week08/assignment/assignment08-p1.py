92afc472f2901742f54d6fe88ff6081b313ca585
jessicapayne95775 <jen19007@byui.edu>
2023-03-27 09:33:42 -0600

    assignment8p1 update

--- week08/assignment/assignment08-p1.py
+++ week08/assignment/assignment08-p1.py
@@ -12,11 +12,60 @@ import sys
 SCREEN_SIZE = 800
 COLOR = (0, 0, 255)
 
+def solve_path_helper(maze: Maze, path, row, col, color):
+    if maze.at_end(row, col):
+        return True
+    
+    moves = maze.get_possible_moves(row, col)
 
-def solve(maze, row, col, solution_path):
+    for x, y in moves:
+        if maze.can_move_here(x, y):
+            maze.move(x, y, color)
+            path.append((x, y))
+
+            if solve_path_helper(maze: Maze, path, x, y, color):
+                return True
+            
+            maze.restore(x,y)
+            path.remove(x, y)
+
+def solve_path(maze: Maze):
     # Mark the current cell as visited
-    maze.move(row, col, COLOR)
+    start = maze.get_start_pos()
+    maze.move(start[0], start[1], COLOR)
+    path = [(start[0], start[1])]
+    solve_path_helper(maze, path, start[0], start[1], COLOR)
+    return path
 
+def get_solution_path(filename):
+    # create a Screen Object that will contain all of the drawing commands
+    screen = Screen(SCREEN_SIZE, SCREEN_SIZE)
+    screen.background((255, 255, 0))
+
+    maze = Maze(screen, SCREEN_SIZE, SCREEN_SIZE, filename)
+
+    row, col = maze.start_pos
+    solution_path = [(row, col)]
+
+    solve_path(maze, row, col, solution_path)
+
+    print(f'Number of drawing commands for {filename}: {screen.get_command_count()}')
+
+    done = False
+    speed = 1
+    while not done:
+        if screen.play_commands(speed):
+            key = cv2.waitKey(0)
+            if key == ord('+'):
+                speed = max(0, speed - 1)
+            elif key == ord('-'):
+                speed += 1
+            elif key != ord('p'):
+                done = True
+        else:
+            done = True
+
+    return solution_path
     # Base case: if we have reached the end position, return True
     if (row, col) == maze.end_pos:
         solution_path.append((row, col))
@@ -42,35 +91,7 @@ def solve(maze, row, col, solution_path):
     return False
 
 
-def get_solution_path(filename):
-    # create a Screen Object that will contain all of the drawing commands
-    screen = Screen(SCREEN_SIZE, SCREEN_SIZE)
-    screen.background((255, 255, 0))
 
-    maze = Maze(screen, SCREEN_SIZE, SCREEN_SIZE, filename)
-
-    row, col = maze.start_pos
-    solution_path = [(row, col)]
-
-    solve(maze, row, col, solution_path)
-
-    print(f'Number of drawing commands for {filename}: {screen.get_command_count()}')
-
-    done = False
-    speed = 1
-    while not done:
-        if screen.play_commands(speed):
-            key = cv2.waitKey(0)
-            if key == ord('+'):
-                speed = max(0, speed - 1)
-            elif key == ord('-'):
-                speed += 1
-            elif key != ord('p'):
-                done = True
-        else:
-            done = True
-
-    return solution_path
 
 
 def find_paths():
